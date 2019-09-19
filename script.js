9f04c91d7d3b2eb6bcd4721132d6a96609190855
burik <dimlelik@rambler.ru>
2019-09-19 20:02:56 +0300

    volume

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const sum = 3051;  //сумма текущей покупки
 const calculation = sum - (sum % 1000);  //расчет бонуса
 const bonus = 100 * (calculation / 1000);
-const resultBonusStr = bonus + " " + "бонусов";  //полученное колличество бонусов
+const resultBonusStr = bonus + " " + "бонусов";  //полученное количество бонусов
 console.log(resultBonusStr);
-console.log('hi');
